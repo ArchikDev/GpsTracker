be8d691366b3f3231311b0d169b92503f14f2678
a.galeev <a.galeev@malltech.ru>
2023-03-23 17:26:53 +0500

    add Extensions functions

--- app/src/main/java/com/archik/gpstracker/MainActivity.kt
+++ app/src/main/java/com/archik/gpstracker/MainActivity.kt
@@ -1,9 +1,14 @@
 package com.archik.gpstracker
 
+import android.media.audiofx.Equalizer.Settings
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.archik.gpstracker.databinding.ActivityMainBinding
+import com.archik.gpstracker.screens.MainFragment
+import com.archik.gpstracker.screens.SettingsFragment
+import com.archik.gpstracker.screens.TrackFragment
+import com.archik.gpstracker.utils.openFragment
 
 class MainActivity : AppCompatActivity() {
   private lateinit var binding: ActivityMainBinding
@@ -15,15 +20,20 @@ class MainActivity : AppCompatActivity() {
 
     setContentView(binding.root)
 
+    startInit()
+  }
+
+  private fun startInit() {
     onBtnNavClick()
+    openFragment(MainFragment.newInstance())
   }
 
   private fun onBtnNavClick() {
     binding.btnNav.setOnItemSelectedListener {
       when(it.itemId) {
-        R.id.id_home -> Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show()
-        R.id.id_tracks -> Toast.makeText(this, "Tracks", Toast.LENGTH_SHORT).show()
-        R.id.id_settings -> Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show()
+        R.id.id_home -> openFragment(MainFragment.newInstance())
+        R.id.id_tracks -> openFragment(TrackFragment.newInstance())
+        R.id.id_settings -> openFragment(SettingsFragment.newInstance())
       }
       true
     }
