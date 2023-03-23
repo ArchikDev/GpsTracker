f57c6889e2c2c563d08155e476512d5ddba86804
a.galeev <a.galeev@malltech.ru>
2023-03-23 16:48:40 +0500

    bottomNav add icon

--- app/src/main/java/com/archik/gpstracker/MainActivity.kt
+++ app/src/main/java/com/archik/gpstracker/MainActivity.kt
@@ -2,6 +2,7 @@ package com.archik.gpstracker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.archik.gpstracker.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,5 +14,18 @@ class MainActivity : AppCompatActivity() {
     binding = ActivityMainBinding.inflate(layoutInflater)
 
     setContentView(binding.root)
+
+    onBtnNavClick()
+  }
+
+  private fun onBtnNavClick() {
+    binding.btnNav.setOnItemSelectedListener {
+      when(it.itemId) {
+        R.id.id_home -> Toast.makeText(this, "Home", Toast.LENGTH_SHORT).show()
+        R.id.id_tracks -> Toast.makeText(this, "Tracks", Toast.LENGTH_SHORT).show()
+        R.id.id_settings -> Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show()
+      }
+      true
+    }
   }
 }
