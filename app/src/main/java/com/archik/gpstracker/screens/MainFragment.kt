32c9f0ea519ea384d0cb9950996463b22e2cfd43
a.galeev <a.galeev@malltech.ru>
2023-03-25 09:28:41 +0500

    add osm

--- app/src/main/java/com/archik/gpstracker/screens/MainFragment.kt
+++ app/src/main/java/com/archik/gpstracker/screens/MainFragment.kt
@@ -1,12 +1,19 @@
 package com.archik.gpstracker.screens
 
+import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import com.archik.gpstracker.R
 import com.archik.gpstracker.databinding.FragmentMainBinding
+import org.osmdroid.config.Configuration
+import org.osmdroid.library.BuildConfig
+import org.osmdroid.util.GeoPoint
+import org.osmdroid.views.overlay.mylocation.GpsMyLocationProvider
+import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay
 
 class MainFragment : Fragment() {
 
@@ -16,9 +23,42 @@ class MainFragment : Fragment() {
     inflater: LayoutInflater, container: ViewGroup?,
     savedInstanceState: Bundle?
   ): View {
+    // Настройка до инициализации карты
+    settingsOsm()
+
     binding = FragmentMainBinding.inflate(inflater, container, false)
 
     return binding.root
+  }
+
+  override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+    super.onViewCreated(view, savedInstanceState)
+
+    initOsm()
+  }
+
+  private fun settingsOsm() {
+    Configuration.getInstance().load(
+      activity as AppCompatActivity,
+      activity?.getSharedPreferences("osm_pref", Context.MODE_PRIVATE)
+    )
+    Configuration.getInstance().userAgentValue = BuildConfig.APPLICATION_ID
+  }
+
+  private fun initOsm() = with(binding) {
+    map.controller.setZoom(20.0)
+
+    val mLocProvider = GpsMyLocationProvider(activity)
+    // создаём слой поверх карты
+    val mLocOverlay = MyLocationNewOverlay(mLocProvider, map)
+    mLocOverlay.enableMyLocation() // моё местоположение
+    mLocOverlay.enableFollowLocation() // следить за местоположением
+    // После того как определилось местположение
+    mLocOverlay.runOnFirstFix {
+      map.overlays.clear()
+      map.overlays.add(mLocOverlay) // Добавляем наш слой
+    }
+
   }
 
   companion object {
